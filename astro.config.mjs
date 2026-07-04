ee96939feb684acda736a3cf2f268b99e49e8744
Lee Muriithi Kingori <leekingori54@gmail.com>
2026-07-04 08:59:47 +0300

    fix: inline styles so Cloudflare cache can't desync CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://leekingori.lestramk.org',
   build: {
-    inlineStylesheets: 'auto',
+    inlineStylesheets: 'always',
   },
   compressHTML: true,
 });
